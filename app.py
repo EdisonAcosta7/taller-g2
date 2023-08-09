baaf04f374587667b91a1d5479d84e7c2b3e5bd1
Edison Acosta <edisson.acosta@gmail.com>
2023-08-08 20:32:44 -0500

    calcula el seno

--- app.py
+++ app.py
@@ -7,6 +7,17 @@ def calcula_cos(value):
 print("Cose-x {}".format(calcula_cos(20)))
 
 
+# Ejemplo 2
+def calcula_sin(value):
+    return math.sin(value)
+
+print("Sen-x {}".format(calcula_sin(20)))
+
+
+
+
+
+
 
 
 
